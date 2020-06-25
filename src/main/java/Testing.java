4d378eb1a5fdf22ee87d51c3e4e3932f92c71205
Lalit Chaudhary <lalit3.kumar@paytm.com>
2020-06-25 14:23:25 +0530

    update com.github.thelalitchaudhary

--- src/main/java/Testing.java
+++
@@ -1,9 +0,0 @@
-import com.addition.Operation;
-
-import java.math.BigDecimal;
-
-public class Testing {
-    public static void main(String[] args) {
-        System.out.println(Operation.add(BigDecimal.valueOf(5.4),BigDecimal.valueOf(6.4)));
-    }
-}
---
+++ src/main/java/com/github/thelalitchaudhary/Sum.java
@@ -0,0 +1,7 @@
+package com.github.thelalitchaudhary;
+
+public class Sum {
+    public int getAdd(int a, int b) {
+       return a + b;
+    }
+}
