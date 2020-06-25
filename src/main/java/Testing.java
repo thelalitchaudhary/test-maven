c6591cef3a8be797cedfe2141b7f8598bce59ea2
Lalit Chaudhary <lalit3.kumar@paytm.com>
2020-06-25 10:11:50 +0530

    first commit

---
+++ src/main/java/Testing.java
@@ -0,0 +1,9 @@
+import com.addition.Operation;
+
+import java.math.BigDecimal;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println(Operation.add(BigDecimal.valueOf(5.4),BigDecimal.valueOf(6.4)));
+    }
+}
